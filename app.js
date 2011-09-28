26c3620a01f5bce4bdd7bc6196538a301ad377ce
ewoudj <ewoudj@gmail.com>
2011-09-28 23:02:02 +0200

    Added feedItem to examples

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var server = require('./private/server/http').createServer(3001, {
     models :[
-         "./examples/models/feed.js"
+         "./examples/models/feed.js",
+         "./examples/models/feeditem.js"
      ]
  });
